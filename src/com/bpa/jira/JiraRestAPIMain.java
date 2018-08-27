55e9060f5e76d096f23f13e95144fac367e08cfc
Manjunath Reddy <bmnr.165@gmail.com>
2018-08-27 12:18:46 +0530

    Update JIRA ticket TRANSITION using REST API - JAVA

--- src/com/bpa/jira/JiraRestAPIMain.java
+++ src/com/bpa/jira/JiraRestAPIMain.java
@@ -2,85 +2,69 @@ package com.bpa.jira;
 
 import com.atlassian.jira.rest.client.api.JiraRestClient;
 import com.atlassian.jira.rest.client.api.JiraRestClientFactory;
+import com.atlassian.jira.rest.client.api.domain.Comment;
 import com.atlassian.jira.rest.client.api.domain.Issue;
 import com.atlassian.jira.rest.client.api.domain.SearchResult;
 import com.atlassian.jira.rest.client.api.domain.User;
+import com.atlassian.jira.rest.client.api.domain.input.TransitionInput;
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
-import com.atlassian.util.concurrent.Promise;
+import com.atlassian.jira.rest.client.internal.async.DelegatingPromise;
 
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.URI;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.TreeSet;
 import java.util.logging.Logger;
 
-import org.apache.commons.lang.StringUtils;
-
 public class JiraRestAPIMain {
-	
-	static Logger logger = Logger.getLogger("JiraRestAPIEx");
-	
-	public static void main(String[] args) {
-		
-		  final String JIRA_URL = args[0]; 
-		  final String JIRA_ADMIN_USERNAME =args[1]; 
-		  final String JIRA_ADMIN_PASSWORD = args[2]; 
-		  String JIRA_USER_SEARCH = args[3];
-		  int START_AT = Integer.parseInt(args[4]); 
-		  int MAX_RESULTS = Integer.parseInt(args[5]); 
-		  String FILE_PATH = args[6]; 
-		  String FILE_NAME = args[7];
 
+	static Logger logger = Logger.getLogger("JiraRestAPIEx");
+	static JiraRestClient client = null;
+
+	public static void main(String[] args) {
+
+		final String JIRA_URL = "URL";
+		final String JIRA_ADMIN_USERNAME = "user Name";
+		final String JIRA_ADMIN_PASSWORD = "Password";
+		final String PROJECT_NAME = "PROJECT";
+		final String UNIQUE_LABEL = "UNIQUE LABEL NAME";
+		final String COMMENT_BODY = "MOVED FROM OPEN TO DEV using JIRA REST API";
+		final int OPEN_TO_DEV_TRANSISTION = 1; // Transition id (Id specifies where this transition should get moved)
 		URI uri;
-		User user;
-		Writer fstream = null;
-		JiraRestClient client = null;
 		try {
-			if(StringUtils.isNotBlank(JIRA_URL) || StringUtils.isNotEmpty(JIRA_URL)){
+			if (JIRA_URL != null) {
 				// Construct the JRJC client
-				logger.info(String.format("Logging in to %s with username '%s' and password '%s'", JIRA_URL,
-						JIRA_ADMIN_USERNAME, JIRA_ADMIN_PASSWORD));
+				String.format("Logging in to %s with username '%s' and password '%s'", JIRA_URL,
+						JIRA_ADMIN_USERNAME, JIRA_ADMIN_PASSWORD);
 				uri = new URI(JIRA_URL);
 				JiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
+				client = factory.createWithBasicHttpAuthentication(uri, JIRA_ADMIN_USERNAME, JIRA_ADMIN_PASSWORD);
+				TreeSet<String> set = new TreeSet<String>();
+				set.add("*all");
+				DelegatingPromise<SearchResult> searchJqlPromise = (DelegatingPromise<SearchResult>) client
+						.getSearchClient().searchJql("project = " + PROJECT_NAME + " AND labels = " + UNIQUE_LABEL
+								+ " ORDER BY priority DESC, updated DESC", 100, 0, set);
 
-				client = factory.createWithBasicHttpAuthentication(uri, JIRA_ADMIN_USERNAME,
-						JIRA_ADMIN_PASSWORD);
-			}
-			
-			// Invoke the JRJC Client
-			Promise<User> promise = client.getUserClient().getUser(JIRA_ADMIN_USERNAME);
-			user = promise.claim();
+				SearchResult searchResult = searchJqlPromise.get();
+				Iterator<Issue> isssue = searchResult.getIssues().iterator();
+				while (isssue.hasNext()) {
+					Issue jiraTicket = isssue.next();
+					System.out.println(jiraTicket.getKey());
+					TransitionInput tinput = new TransitionInput(OPEN_TO_DEV_TRANSISTION);
+					client.getIssueClient().transition(jiraTicket, tinput).claim();
+					JiraRestAPIMain.addComment(jiraTicket, COMMENT_BODY);
 
-			TreeSet<String> set = new TreeSet<String>();
-			set.add("*all");
-			ArrayList<String> userEmailList = new ArrayList<String>();
-			userEmailList.add(user.getEmailAddress());
-			
-			// JIRA_USER_SEARCH is search user on behalf of the user.
-			// User X can search his results or User Y assigned results.
-			Promise<SearchResult> searchJqlPromise = client.getSearchClient().searchJql(
-					"assignee in ("+JIRA_USER_SEARCH+") ORDER BY priority DESC, updated DESC", MAX_RESULTS, START_AT, set);
-			fstream = new OutputStreamWriter(new FileOutputStream(FILE_PATH+"\\"+FILE_NAME), StandardCharsets.UTF_8);
-			logger.info(searchJqlPromise.claim().getTotal() + "");
-			for (Issue issue : searchJqlPromise.claim().getIssues()) {
-				logger.info(issue.getKey() + " : " + issue.getId());
-				fstream.write(issue.getKey() + ":" + issue.getId());
-				fstream.write(System.getProperty( "line.separator" ));
-				fstream.flush();
+				}
 			}
-			if (null != fstream) {
-				fstream.close();
-			}
-			logger.info(String.format("Your admin user's email address is: %s\r\n",
-					user.getEmailAddress() + " : " + " : " + user.getDisplayName()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		System.exit(0);
+	}
+
+	public static void addComment(Issue issue, String commentBody) {
+		client.getIssueClient().addComment(issue.getCommentsUri(), Comment.valueOf(commentBody));
 	}
 
 }
